cabef24d6b4fa07c37f33b137fe12deeeadf114a
Zephyr04 <42781435+Zephyr04@users.noreply.github.com>
2018-11-06 21:06:13 -0500

    Add files via upload

---
+++ hangman.java
@@ -0,0 +1,103 @@
+public class hangman {
+
+    public static void main(String[]args){
+
+        System.out.println("________________");
+        System.out.println("|      |       |");
+        System.out.println("               |");
+        System.out.println("               |");
+        System.out.println("               |");
+        System.out.println("_______________|");
+
+
+        System.out.println("________________");
+        System.out.println("|      |       |");
+        System.out.println("       0       |");
+        System.out.println("               |");
+        System.out.println("               |");
+        System.out.println("_______________|");
+
+        System.out.println("________________");
+        System.out.println("|      |       |");
+        System.out.println("       0       |");
+        System.out.println("       |       |");
+        System.out.println("               |");
+        System.out.println("_______________|");
+
+
+        System.out.println("________________");
+        System.out.println("|      |       |");
+        System.out.println("       0       |");
+        System.out.println("       |-      |");
+        System.out.println("               |");
+        System.out.println("_______________|");
+
+        System.out.println("________________");
+        System.out.println("|      |       |");
+        System.out.println("       0       |");
+        System.out.println("      -|-      |");
+        System.out.println("               |");
+        System.out.println("_______________|");
+
+        System.out.println("________________");
+        System.out.println("|      |       |");
+        System.out.println("       0       |");
+        System.out.println("      -|-      |");
+        System.out.println("      /        |");
+        System.out.println("_______________|");
+
+
+        System.out.println("________________");
+        System.out.println("|      |       |");
+        System.out.println("       0       |");
+        System.out.println("      -|-      |");
+        System.out.println("      /|       |");
+        System.out.println("_______________|");
+
+
+        System.out.println("\n" +
+                "_____________________  ____________   __________    __________________     \n" +
+                "__  ____/__    |__   |/  /__  ____/   __  __ \\_ |  / /__  ____/__  __ \\    \n" +
+                "_  / __ __  /| |_  /|_/ /__  __/      _  / / /_ | / /__  __/  __  /_/ /    \n" +
+                "/ /_/ / _  ___ |  /  / / _  /___      / /_/ /__ |/ / _  /___  _  _, _/     \n" +
+                "\\____/  /_/  |_/_/  /_/  /_____/      \\____/ _____/  /_____/  /_/ |_|      \n" +
+                "                                                                           \n");
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
